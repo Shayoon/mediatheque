fe81e5eabf7eed9c7d9dd4163783b9738673c204
Quentin Degorgue <104148133+Shayoon@users.noreply.github.com>
2022-06-23 16:00:07 +0200

    Revert "Feature nav bar footer"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NotFoundComponent } from './commons/not-found/not-found.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { ProductsPageComponent } from './pages/products-page/products-page.component';
 
-const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: "full"},
-  {path: 'home', component: HomePageComponent},
-  {path: 'product', component: ProductsPageComponent},
-  {path: '**', component: NotFoundComponent}, //Toujours mettre ce rooting à la fin  
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatButtonModule} from '@angular/material/button';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +8,6 @@ import { FooterComponent } from './commons/footer/footer.component';
 import { NotFoundComponent } from './commons/not-found/not-found.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductsPageComponent } from './pages/products-page/products-page.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductCardComponent } from './components/product-card/product-card.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -22,14 +17,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     NotFoundComponent,
     HomePageComponent,
     ProductsPageComponent,
-    ProductListComponent,
-    ProductCardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

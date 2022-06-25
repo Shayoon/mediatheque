62eba6279994dd5d4665f3eaa394aafc195bebd5
Shayoon <quentin.degorgue@gmail.com>
2022-06-25 08:58:00 +0200

    fix: Default product image

--- src/app/pages/products-page/product-card/product-card.component.ts
+++ src/app/pages/products-page/product-card/product-card.component.ts
@@ -9,7 +9,7 @@ export class ProductCardComponent implements OnInit {
   @Input()
   produit: any;
   
-  DEFAULT_IMG : string = "https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fthumbs.dreamstime.com%2Fb%2Funicorn-with-colorful-hear-on-rainbow-background-147517458.jpg&sp=1656053747Tfbe1d37e0a76991b95180470a251f265dab045609b264686d6af1dda3b95ab48"
+  DEFAULT_IMG : string = "https://parfums-ugo.fr/wp-content/uploads/2021/07/Licorne.jpeg"
 
   constructor() { }
 
